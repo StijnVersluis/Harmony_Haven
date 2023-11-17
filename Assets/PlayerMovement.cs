162c94eb2371d2d3bbba814e02afc3c03a3b66ff
Stijn Versluis <stijn.versluis@gmail.com>
2023-11-17 12:21:21 +0100

    Add player movement and borders

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public float playerSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float horizontalDirection = Input.GetAxis("Horizontal");
+        float verticalDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalDirection, verticalDirection, 0f) * playerSpeed * Time.deltaTime;
+
+        transform.Translate(moveDirection);
     }
 }
